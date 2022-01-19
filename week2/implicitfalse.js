11c5a40f02a72a65e40e63c20d5d71b5a9bd630f
Bossinwza007 <6306021620026@fitm.kmutnb.ac.th>
2022-01-19 11:57:55 +0700

    update

---
+++ week2/implicitfalse.js
@@ -0,0 +1,7 @@
+const name='';
+
+if(name){
+    console.log('we have a name!');
+}else{
+    console.log('no name provided')
+}
---
+++ week2/varletconst.js
@@ -0,0 +1,13 @@
+var hello ="Hello";
+
+console.log(hello);
+hello=("Hello World");
+console.log(hello);
+
+if (true){
+    let world = "Hello World";
+    console.log(world);
+}
+
+const fixval ="fixvalue";
+console.log(fixval);
