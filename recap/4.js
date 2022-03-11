4ca04d6fc29e9bdeea9731ae7472cd45699d6bf5
Bossinwza007 <6306021620026@fitm.kmutnb.ac.th>
2022-03-12 02:34:53 +0700

    .

--- recap/4.js
+++ recap/4.js
@@ -1,8 +1,20 @@
-function totalVolume(a,b,c){
-    
-
+function totalVolume(...num){
+    var multiply = 1;
+    var total = 0;
+    for(var i = 0; i < num.length; i++){
+        for(var j = 0; j < 3; j++){
+            multiply *= num[i][j];
+        }
+        total += multiply
+        if(j == 3){
+            multiply = 1
+        }
+    }
+    return total
 }
 
-console.log(totalVolume([4,2,4],[3,3,3],[1,1,2],[2,1,1]))
-console.log(totalVolume([2,2,2],[2,1,1]))
-console.log(totalVolume([1,1,1]))
+
+console.log(totalVolume([4, 2, 4], [3, 3, 3], [1, 1, 2], [2, 1, 1]));
+console.log(totalVolume([2, 2, 2], [2, 1, 1]));
+console.log(totalVolume([1, 1, 1]));
+
