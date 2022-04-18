b5d68df1eafb90138e131e165ce9b87a54b1a14d
Bossinwza007 <6306021620026@fitm.kmutnb.ac.th>
2022-04-18 20:58:52 +0700

    update

--- borntodev-project/index.js
+++ borntodev-project/index.js
@@ -3,13 +3,13 @@ window.onload = () => {
     button.addEventListener("click", calculate);
 }
 
-function calculate(){
+function calculate() {
     let degree = document.querySelector("#degree").value;
     let result = document.querySelector("#result")
     let sin = Math.sin(degree)
     let cos = Math.cos(degree)
-    let tan = (sin/cos)
-    result.innerHTML=`<br>Result Sin: ${sin}<br>
+    let tan = (sin / cos)
+    result.innerHTML = `<br>Result Sin: ${sin}<br>
     <br>Result Cos: ${cos}<br>
     <br>Result Tan: ${tan}`
 }
